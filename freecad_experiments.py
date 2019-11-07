adce9fe5b4c1fcfe8c1b45506ba155fd60a5cd9d
Eli Dupree <vcs@elidupree.com>
2019-11-07 18:21:52 -0500

    more work on the snapper (but now the solver can't solve it, we need a different approach)

--- freecad_experiments.py
+++ freecad_experiments.py
@@ -28,10 +28,11 @@ band_width = 6
 claw_length = band_width + 2
 flex_length = 40
 flex_thickness = 1
-claw_thickness = 1
+claw_thickness = 2
 motion_distance = 100
 channel_depth = 6
-deflector_peg_diameter = 3
+deflector_peg_diameter = 2
+deflector_thickness = 1
 
 deflector_peg_radius = deflector_peg_diameter/2
 
@@ -40,9 +41,11 @@ document().addObject ("Sketcher::SketchObject", "Sketch")
 #document().Sketch.Support = (App.activeDocument().XY_Plane, [''])
 #document().Sketch.MapMode = "FlatFace"
 current_sketch = "Sketch"
-def add_line(*constraints):
+
+hack_offset = 0
+def add_geometry(geometry, *constraints):
   index = document().getObject (current_sketch).GeometryCount
-  document().getObject (current_sketch).addGeometry (Part.LineSegment (FreeCAD.Vector (-5+index, 23+index), FreeCAD.Vector (-5+index, -4+index)))
+  document().getObject (current_sketch).addGeometry (geometry, False)
   if index > 0:
     add_constraint ("Coincident", index - 1, 2, index, 1);
   for constraint in constraints:
@@ -53,11 +56,41 @@ def add_line(*constraints):
     constraint.insert(1, index)
     add_constraint (*constraint);
   return index
+def add_line(*constraints):
+  global hack_offset
+  hack_offset += 1
+  line = Part.LineSegment (FreeCAD.Vector (-5+hack_offset, 23+hack_offset), FreeCAD.Vector (-5+hack_offset*2, -4+hack_offset))
+  return add_geometry(line, *constraints)
+def add_arc(*constraints):
+  global hack_offset
+  hack_offset += 1
+  arc = Part.ArcOfCircle(Part.Circle(FreeCAD.Vector (-5+hack_offset, 23+hack_offset*1.1, 0), FreeCAD.Vector (0,0,1), 2+hack_offset), 2+hack_offset*1.2, 3+hack_offset*1.3)
+  return add_geometry(arc, *constraints)
 def add_constraint(*args):
   document().getObject (current_sketch).addConstraint(Sketcher.Constraint (*args))
 
+slider_bottom = add_line ("Horizontal")
+slider_left = add_line ("Vertical", ("Distance", channel_depth))
+slider_top = add_line ("Horizontal")
+flex_inner_arc = add_arc()
+flex_bottom = add_line("Horizontal")
+peg_right = add_line ("Vertical")
+peg_bottom = add_line ("Horizontal")
+claw_left = add_line("Vertical", ("Distance", claw_length + deflector_thickness + deflector_peg_diameter))
+claw_top = add_line ("Horizontal", ("Distance", claw_thickness))
 claw_right = add_line("Vertical", ("Distance", claw_length))
 flex_top = add_line("Horizontal", ("Distance", flex_length))
+flex_outer_arc = add_arc()
+
+add_constraint ("Coincident", flex_outer_arc, 2, slider_bottom, 1)
+
+add_constraint ("DistanceX", flex_bottom, 1, flex_top, 2, 0);
+add_constraint ("DistanceX", flex_bottom, 2, flex_top, 1, 0);
+add_constraint ("DistanceY", flex_top, 1, flex_bottom, 1, flex_thickness);
+add_constraint ("DistanceY", peg_bottom, 1, slider_top, 1, claw_length);
+#add_constraint ("DistanceY", peg_bottom, 1, flex_top, 1, deflector_thickness + deflector_peg_diameter);
+add_constraint ("Tangent", flex_outer_arc, flex_top);
+
 
 #object = document.addObject("Part::Box","Box")
 #object.Label = "Cube"
