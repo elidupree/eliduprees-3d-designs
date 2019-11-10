4b50f1471652efc064861650bb218bc58c1f98d7
Eli Dupree <vcs@elidupree.com>
2019-11-09 22:32:35 -0500

    more work on snapper

--- freecad_experiments.py
+++ freecad_experiments.py
@@ -35,6 +35,7 @@ App.newDocument("Something")
 band_width = 6
 band_thickness = 1
 band_leeway = 1
+flex_perpendicular_leeway = 2
 claw_length = band_width + 2
 claw_deflect_distance = claw_length + 1
 claw_arm_length = 40
@@ -42,7 +43,7 @@ claw_solid_length = 18
 flex_length = 12
 flex_thickness = 1
 flex_support_length = 6
-claw_thickness = 2
+claw_thickness = 2.5
 claw_width = 6
 motion_distance = 100
 channel_depth = 6
@@ -52,6 +53,7 @@ deflector_peg_diameter = 2
 deflector_peg_length = 2
 deflector_thickness = 1
 deflector_slope = 0.2
+releaser_slope = 0.6
 
 deflector_peg_radius = deflector_peg_diameter/2
 deflector_radius = deflector_thickness/2
@@ -69,6 +71,8 @@ flex_top = 0
 claw_top = flex_top + claw_length
 flex_bottom = flex_top - flex_thickness
 claw_solid_bottom = flex_top - claw_thickness
+deflector_top_center = flex_top - deflector_radius
+deflector_fully_down_center = deflector_top_center - claw_deflect_distance
 deflector_peg_bottom = flex_top - deflector_thickness - deflector_peg_diameter - slider_channel_tolerance
 slider_top = deflector_peg_bottom - claw_deflect_distance - slider_channel_tolerance
 slider_bottom = slider_top - channel_depth
@@ -85,18 +89,22 @@ slider_protrusions_back = claw_back + deflector_peg_length
 
 channel_left_stop = claw_left
 channel_right_stop = flex_support_right + motion_distance
-deflector_left_end = deflector_peg_right
+deflector_left_end = deflector_peg_right + flex_perpendicular_leeway
 deflector_left_end_center = deflector_left_end + deflector_radius
-deflector_fully_down = claw_right + band_thickness*2 + band_leeway*2 + (deflector_peg_horizontal_middle - claw_left)
-deflector_right_end_center = deflector_fully_down + claw_deflect_distance/deflector_slope
-releaser_fully_down = claw_left + motion_distance
-releaser_left_end_center = releaser_fully_down - claw_deflect_distance/deflector_slope
+deflector_fully_down_horizontal = claw_right + band_thickness*2 + band_leeway*2 + (deflector_peg_horizontal_middle - claw_left)
+deflector_right_end_center = deflector_fully_down_horizontal + claw_deflect_distance/deflector_slope
+releaser_fully_down_horizontal = deflector_peg_horizontal_middle + motion_distance
+releaser_left_end_center = releaser_fully_down_horizontal - claw_deflect_distance/releaser_slope
 
 slider_shape = FreeCAD_shape_builder (lambda whatever: whatever + vector (0, 0, claw_front)).build ([
   start_at (flex_support_right, slider_bottom),
   horizontal_to (claw_left), vertical_to (slider_top), horizontal_to (flex_right), vertical_to (flex_bottom), horizontal_to (flex_left), diagonal_to (claw_solid_right, claw_solid_bottom), horizontal_to (deflector_peg_right),
-  vertical_to (deflector_peg_bottom + deflector_peg_radius),
-  arc_through_to ((deflector_peg_horizontal_middle, deflector_peg_bottom), (deflector_peg_left, deflector_peg_bottom + deflector_peg_radius)),
+  
+  #vertical_to (deflector_peg_bottom + deflector_peg_radius),
+  #arc_through_to ((deflector_peg_horizontal_middle, deflector_peg_bottom), (deflector_peg_left, deflector_peg_bottom + deflector_peg_radius)),
+  horizontal_to (deflector_peg_horizontal_middle),
+  diagonal_to (claw_left, deflector_peg_bottom + deflector_peg_radius),
+  
   vertical_to (claw_top), horizontal_to (claw_right), vertical_to (flex_top), horizontal_to (flex_support_right), close()
 ])
 
@@ -145,7 +153,14 @@ edges = [edge
 
 slider_part = slider_part.makeFillet(1, edges) #[slider_part.Edges [index] for index in range (0, len (slider_part.Edges), 3)])
 
-
+channel_box = Part.makeBox (
+  channel_right_stop - channel_left_stop + channel_wall_thickness,
+  flex_top - slider_bottom + channel_wall_thickness,
+  slider_protrusions_back - slider_protrusions_front + channel_wall_thickness*2)
+channel_box.translate (vector (
+  channel_left_stop - channel_wall_thickness,
+  slider_bottom - channel_wall_thickness,
+  slider_protrusions_front - channel_wall_thickness))
 
 wide_channel_shape = FreeCAD_shape_builder (lambda whatever: vector (channel_left_stop, whatever [1], whatever [0])).build ([
   start_at(claw_front, slider_top),
@@ -179,20 +194,32 @@ narrow_channel_wire = Part.Wire (narrow_channel_shape.Edges)
 narrow_channel_wire = narrow_channel_wire.makeOffset2D (slider_channel_tolerance)
 narrow_channel_part = Part.Face (narrow_channel_wire).extrude (FreeCAD.Vector (channel_right_stop - channel_left_stop, 0, 0))
 
-body_part = Part.makeBox (
-  channel_right_stop - channel_left_stop + channel_wall_thickness,
-  flex_top - slider_bottom + channel_wall_thickness,
-  slider_protrusions_back - slider_protrusions_front + channel_wall_thickness*2)
-body_part.translate (vector (
-  channel_left_stop - channel_wall_thickness,
-  slider_bottom - channel_wall_thickness,
-  slider_protrusions_front - channel_wall_thickness))
-body_part = body_part.cut(wide_channel_part).cut(narrow_channel_part)
+deflector_shape = FreeCAD_shape_builder (lambda whatever: whatever + vector (0, 0, slider_protrusions_front - channel_wall_thickness)).build ([
+  start_at (deflector_left_end_center, deflector_fully_down_center),
+  horizontal_to (deflector_fully_down_horizontal),
+  diagonal_to (deflector_right_end_center, deflector_top_center),
+  #start_at (channel_right_stop, deflector_fully_down_center),
+  #horizontal_to (releaser_fully_down_horizontal),
+  #diagonal_to (releaser_left_end_center, deflector_top_center),
+])
+deflector_wire = Part.Wire (deflector_shape.Edges).makeOffset2D (deflector_radius)
+deflector_part = Part.Face (deflector_wire).extrude (FreeCAD.Vector (0, 0, slider_protrusions_back - slider_protrusions_front + channel_wall_thickness*2))
+
+releaser_shape = FreeCAD_shape_builder (lambda whatever: whatever + vector (0, 0, slider_protrusions_front - channel_wall_thickness)).build ([
+  start_at (channel_right_stop, deflector_fully_down_center),
+  horizontal_to (releaser_fully_down_horizontal),
+  diagonal_to (releaser_left_end_center, deflector_top_center),
+])
+releaser_wire = Part.Wire (releaser_shape.Edges).makeOffset2D (deflector_radius)
+releaser_part = Part.Face (releaser_wire).extrude (FreeCAD.Vector (0, 0, slider_protrusions_back - slider_protrusions_front + channel_wall_thickness*2)).common (channel_box)
+
+
+body_part = channel_box.cut(wide_channel_part).fuse ((deflector_part, releaser_part)).cut(narrow_channel_part)
 
 
 Part.show (slider_part, "SliderSquare")
 
-#Part.show (body_part)
+Part.show (body_part)
 
 '''document().addObject ("Part::Fillet", "Slider")
 document().Slider.Base = document().SliderSquare
