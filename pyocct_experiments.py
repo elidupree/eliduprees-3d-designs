fdf404e160deb9c856567a24578c3b6c51226898
Eli Dupree <vcs@elidupree.com>
2020-07-26 13:41:08 -0400

    Possibly completed caching system

--- pyocct_experiments.py
+++ pyocct_experiments.py
@@ -1,4 +1,5 @@
 import pprint
+import sys
 import OCCT.BOPAlgo
 import OCCT.BRepAlgoAPI
 import OCCT.BRepBuilderAPI
@@ -25,6 +26,21 @@ print(OCCT.BRepBuilderAPI.BRepBuilderAPI_MakeFace(6))
 '''
 
 import dis
-g=5
-repr(str(dis.dis(pprint.pprint)))
+g=7
+transitive_dependency = 6
+print(repr(str(dis.Bytecode(pprint.pprint).dis())))
 print (type (globals()))
+
+from pyocct_system import *
+print (sys.argv)
+initialize_system (globals(), sys.argv[1])
+
+def dependency_function():
+  transitive_dependency
+  pass
+
+@cached("JSON")
+def test():
+  dependency_function()
+  return g
+print (test)
--- pyocct_system.py
+++ pyocct_system.py
@@ -1,12 +1,14 @@
 import datetime
 import hashlib
 import dis
+import inspect
 import functools
 import re
 import os
 import os.path
+import json
 from OCCT.BOPAlgo import BOPAlgo_Options
-from OCCT.Exchange
+import OCCT.Exchange
 
 BOPAlgo_Options.SetParallelMode_(True)
 
@@ -45,6 +47,7 @@ _cache_info_by_global_key = {}
 
 def initialize_system (cache_globals, cache_directory):
   global _cache_globals
+  global _cache_directory
   
   if type (cache_globals) is not dict:
     raise RuntimeError ("called initialize_system without giving globals")
@@ -55,107 +58,159 @@ def initialize_system (cache_globals, cache_directory):
   _cache_directory = cache_directory
 
 
+def _globals_in_code(code):
+  return (match.group (1) for match in re.finditer(r"LOAD_GLOBAL\s+\d+\s+\(([^\)]+)\)", code))
 
-
-def _load_cache (path):
-  path = os.path.abspath(path)
-  base, ext = os.path.splitext(path)
+class Recursive:
+  pass
+class OutputHashError(Exception):
+  pass
+def _output_hash (key):
+  #print("output_hash called", key)
+  in_memory = _cache_info_by_global_key.get(key)
+  if in_memory is Recursive:
+    raise OutputHashError("the system currently can't handle recursive functions")
+  if in_memory is not None:
+    return in_memory ["output_hash"]
   
-  if ext == ".json":
-    with open(path) as file:
+  _cache_info_by_global_key [key] = Recursive
+  
+  if key not in _cache_globals:
+    raise OutputHashError("the system currently can't handle references to global keys that don't exist yet")  
+  value = _cache_globals [key]
+  
+  try:
+    code = dis.Bytecode(value).dis()
+    #print(key, code)
+    code2 = inspect.getsource (value)
+    hasher = hashlib.sha256()
+    #hasher.update (code.encode ("utf-8"))
+    hasher.update (code2.encode ("utf-8"))
+    for key2 in _globals_in_code (code):
+      hasher.update (_output_hash (key2).encode ("utf-8"))
+    result = hasher.hexdigest()
+  except TypeError:
+    result = repr(value)
+    
+  #print(f"Info: decided that output hash of {key} is {result}")
+    
+  _cache_info_by_global_key [key] = {"output_hash": result}
+  return result
+
+def _info_path (key):
+  path = os.path.join (_cache_directory, key)
+  return path + ".cache_info"
+
+def _load_cache (key, kind):
+  path = os.path.join (_cache_directory, key)
+  
+  if kind == "JSON":
+    with open(path + ".json") as file:
       result = json.load(file)
-  elif ext == ".brep":
-    result = ExchangeBasic.read_brep(path)
+  elif kind == "BREP":
+    result = _ExchangeBasic.read_brep(path + ".brep")
   else:
-    raise RuntimeError(f"{ext} filetype not currently supported by pyocct_system.load_cache")
+    raise RuntimeError(f"{kind} isn't a supported kind for pyocct_system caching")
   
   return result
 
-def _save_cache (path, value, inputs_path, inputs):
-  path = os.path.abspath(path)
-  base, ext = os.path.splitext(path)
+def _save_cache (key, kind, value, info):
+  path = os.path.join (_cache_directory, key)
+  info_path = _info_path (key)
   
   # remove the inputs record first so that, in case of the process being terminated, we don't
   #   leave the new mismatched values alongside the old valid inputs;
   # then put the inputs back last so we can't leave the new valid inputs alongside
   #   a broken value
-  os.remove (inputs_path)
-  
-  temp_path = base + ".temp"
-  if ext == ".json":
+  try:
+    os.remove (info_path)
+  except FileNotFoundError:
+    pass
+    
+  temp_path = path + ".temp"
+  if kind == "JSON":
+    value_path = path + ".json"
     with open(temp_path, "w") as file:
       json.dump (value, file)
       file.flush()
       os.fsync(file.fileno())
-  elif ext == ".brep":
+  elif kind == "BREP":
+    value_path = path + ".brep"
     ExchangeBasic.write_brep(value, temp_path)
   else:
     raise RuntimeError(f"{ext} filetype not currently supported by pyocct_system.cached")
     
-  os.replace (temp_path, path)
+  os.replace (temp_path, value_path)
   
   with open(temp_path, "w") as file:
-    json.dump (inputs, file)
+    json.dump (info, file)
     file.flush()
     os.fsync(file.fileno())
-  os.replace (temp_path, inputs_path)
+  os.replace (temp_path, info_path)
+
   
-def _globals_in_code(code):
-  return (match.group (1) for match in re.finditer(r"LOAD_GLOBAL\s+\d+\s+\(([^\)]+)\)", code))
-  
-def _was_updated(global_key):
-  stored = _cache_info_by_global_key.get(global_key)
-  if stored is not None:
-    return stored ["was_updated"]
+
   
   
-def _cache_is_valid (inputs_path, source_hash):
+def _cache_is_valid (key, source_hash):
+  info_path = _info_path (key)
   try:
-    with open(inputs_path) as file:
-      stored_inputs = file.read()
-      if stored_inputs["source_hash"] != source_hash:
+    with open(info_path) as file:
+      stored = json.load(file)
+      if stored["source_hash"] != source_hash:
         return False
-      for key, value in stored_inputs["globals"].items():
-        cache_info = _cache_info_by_global_key.get(key)
-        if cache_info is not None:
-          if cache_info["was_updated"]:
+      for key2, value in stored["globals"].items():
+        try: 
+          if _output_hash (key2) != value:
             return False
-        else:
-          if _cache_globals [key] != value:
-            return False
+        except OutputHashError:
+          return False
         
-  except FileNotFoundError:
+  except (FileNotFoundError, json.decoder.JSONDecodeError):
     return False
     
   return True
 
-def _get_cached(path, generate):
-  path = os.path.abspath(path)
-  base, ext = os.path.splitext(path)
-  code = dis.dis(generate)
-  source_hash = hashlib.sha256(code).hexdigest()
-  inputs_path = base + ".inputs"
-  cache_info = {"was_updated": False}
-  if !_cache_is_valid (inputs_path, source_hash):
-    cache_info["was_updated"] = True
+def _get_cached(key, kind, generate):
+  print (f"### doing {key} ###")
+  code = dis.Bytecode(generate).dis()
+  code2 = inspect.getsource (generate)
+  #print (code)
+  #print(list(_globals_in_code(code)))
+  #print (code2)
+  hasher = hashlib.sha256()
+  #hasher.update (code.encode ("utf-8"))
+  hasher.update (code2.encode ("utf-8"))
+  source_hash = hasher.hexdigest()
+  
+  if _cache_is_valid (key, source_hash):
+    print(f"cached version seems valid, loading it")
+  else:
+    start_time = datetime.datetime.now()
+    print(f"needs update, generating new version… ({start_time})")
     new_result = generate()
-    inputs = {
+    cache_info = {
       "source_hash": source_hash,
-      "globals": {key:_cache_globals [key] for key in _globals_in_code(code)},
+      "globals": {key2: _output_hash(key2) for key2 in _globals_in_code(code)},
     }
-    save_cache (path, new_result, inputs_path, inputs)
+    output_hash = hashlib.sha256 (json.dumps (cache_info).encode ("utf-8")).hexdigest()
+    cache_info ["output_hash"] = output_hash
+    
+    _save_cache (key, kind, new_result, cache_info)
+    finish_time = datetime.datetime.now()
+    print(f"…done! ({finish_time}, took {(finish_time - start_time)})")
     
   
   # note: always reload the cache instead of using the one that was just generated in memory,
   # to make sure it is properly canonicalized
-  return cache_info, _load_cache (path)
+  return _load_cache (key, kind)
     
     
-def cached(path):
+def cached(kind):
   def decorate(generate):
-    cache_info, value = _get_cached(path, generate)
-    _cache_info_by_global_key[generate.__name__] = cache_info
-    return value
+    key = generate.__name__
+    return _get_cached(key, kind, generate)
+  return decorate
 
 
       
