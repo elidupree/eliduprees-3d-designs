3d36ed03dd060f5ffa8c5fd95172bf3511ffb1c1
elidupree <vcs@elidupree.com>
2021-08-21 13:57:10 -0400

    prototyped a version of the HEPA filter plate with prongs to test a "hinge closed whole plate" sort of design

--- air_adapters_pyocct.py
+++ air_adapters_pyocct.py
@@ -15,24 +15,24 @@ strong_filter_width = 101
 def elidupree_4in_wire(offset):
   return Wire (Edge (Circle (Axes (Point (0,0,80) + Up*offset, Up), elidupree_4in_output_outer_radius)))
 
-def strong_filter_wire(offset, outset):
+def strong_filter_wire(outset):
   
-  length = strong_filter_length/2 + outset - offset*0.14
-  width = strong_filter_width/2 + outset - offset*0.14
+  length = strong_filter_length/2 + outset
+  width = strong_filter_width/2 + outset
   return Wire(
     Point (-length, -width, 0),
     Point (length, -width, 0),
     Point (length, width, 0),
     Point (-length, width, 0),
     loop = True,
-  )@Translate(Up*offset)
+  )
 
 
 @run_if_changed
 def make_hepa_to_4in():
   wall_thickness = 0.5
   sections = (
-    [strong_filter_wire(offset, wall_thickness) for offset in subdivisions(-2, 8, amount = 5)]
+    [strong_filter_wire(wall_thickness - offset*0.14)@Translate(Up*offset) for offset in subdivisions(-2, 8, amount = 5)]
     + [elidupree_4in_wire(offset) for offset in subdivisions(-20, 0, amount = 5)]
   )
   solid = Loft(sections, solid=True)
@@ -42,26 +42,45 @@ def make_hepa_to_4in():
   
 @run_if_changed
 def make_hepa_to_nothing():
-  wall_thickness = 1.0
+  wall_thickness = 0.6
+  plate_thickness = 3
   sections = [
-    strong_filter_wire(-5, wall_thickness),
-    strong_filter_wire(0, wall_thickness),
-    strong_filter_wire(0, -5),
+    strong_filter_wire(wall_thickness + 5*0.14)@Translate(Up*5),
+    strong_filter_wire(wall_thickness),
+    strong_filter_wire(-5),
   ]
   loft = Loft(sections[0:2], ruled = True)
+  '''
   faces = loft.faces() + [
     Face (sections [1], holes = sections [2].complemented()),
   ]
   pointy_shell = Shell (faces)
   shell = pointy_shell
+  '''
   '''Fillet(pointy_shell, [(edge, 1.0 + wall_thickness) for edge in 
     sections [1].edges() + [edge for edge in loft.edges() if not all_equal (vertex [2] for vertex in edge.vertices())]
   ])'''
-  preview (shell)
-  preview (Offset (shell, -wall_thickness, fill = False))
-  solid = Offset (shell, -wall_thickness, fill = True)
+  #preview (shell)
+  #preview (Offset (shell, -wall_thickness, fill = False))
+  #solid = Offset (shell, -wall_thickness, fill = True)
+  
+  wall = Offset (Shell(loft.faces()), wall_thickness, fill = True)
+  plate_top = Face (sections [1], holes = sections [2].complemented())
+  plate = plate_top.extrude(Up*0.1, Down*plate_thickness)
+  
+  peg_width = 5
+  peg_length = 15
+  peg_thickness = 4
+  peg = Vertex(Origin).extrude(Right*peg_length).extrude(Up*peg_thickness).extrude(Back*peg_width, centered=True) @ Translate(Down*plate_thickness)
+  center_peg = peg @ Translate(Right*(strong_filter_length/2 + wall_thickness))
+  corner_peg = center_peg @ Translate(Front*(strong_filter_width/2 + wall_thickness - peg_width / 2))
+  right_pegs = Compound(center_peg, corner_peg, corner_peg @ Mirror(Back))
+  
+  solid = Compound(wall, plate, right_pegs, right_pegs@Mirror(Left))
+  
   save ("hepa_to_nothing", solid)
   save_STL("hepa_to_nothing", solid)
+  preview(solid)
   
 @run_if_changed
 def make_hepa_to_nothing_clips():
