278c0104b13b837c3ad3ef78f6877b3ad81c2eaf
elidupree <vcs@elidupree.com>
2022-02-17 20:30:34 -0500

    work on emg mouse enclosure

--- emg_mouse_enclosure.py
+++ emg_mouse_enclosure.py
@@ -5,8 +5,7 @@ initialize_system (globals())
 
 
 printed_wall_thickness = 0.6
-
-flat_wall_thickness = 1.0
+contact_leeway = 0.4
 
 
 sensor_width = 21.9
@@ -18,6 +17,9 @@ min_sensor_spacing = sensor_thickness_with_connectors + space_below_sensor_induc
 controller_width = 31.1
 controller_length = 38.7
 controller_board_thickness = 0.9
+ports_slot_width = 10
+porthole_depth = 5
+porthole_width = porthole_depth*2
 
 controller_contact_inset = 1.2
 controller_contact_spacing = 22.5/9
@@ -71,6 +73,40 @@ signal_wires = [
   for index, (back, source) in enumerate (zip (sensor_backs, signal_sources))
 ]
 
+a = Point (- controller_width/2, controller_back, 0)
+b = Point (- sensor_width/2, sensor_backs [-1] - sensor_board_thickness, 0)
+c = Point (sensor_width/2, sensor_backs [-1] - sensor_board_thickness, 0)
+d = Point (controller_width/2, controller_back, 0)
+wall_curve_points = [Between(a, b, amount) + Left*1.2 for amount in subdivisions (-0.25, 1.2, amount=5)] + [Between(c, d, amount) + Right*(3.4 + porthole_depth) for amount in subdivisions (-0.2, 1.25, amount=3)]
 
-preview (controller_board, sensor_boards, power_wire, ground_wire, signal_wires)
+
+wall_curve = BSplineCurve (wall_curve_points, BSplineDimension (periodic = True))
+wall_curve_length = wall_curve.length()
+ankle_start_distance = wall_curve.distance (closest =a)
+ankle_finish_distance = wall_curve.distance (closest =b)
+
+perforated_length = ankle_start_distance - ankle_finish_distance
+num_portholes = round (perforated_length/porthole_width)
+
+print (ankle_start_distance, ankle_finish_distance, wall_curve_length, perforated_length)
+
+def perforated_control_points (layer):
+  def control_point (distance):
+    derivatives = wall_curve.derivatives (distance = distance)
+    perforated_fraction = (distance - ankle_finish_distance)/perforated_length
+    if 0 < perforated_fraction < 1:
+      portholes_past = perforated_fraction*num_portholes
+      phase = portholes_past % 1
+      portholes_past = math.floor(portholes_past)
+      if portholes_past % 2 == 0:
+        depth = math.sqrt(math.sin(phase * math.pi))*porthole_depth
+        inwards = derivatives.tangent @ Rotate(Up, degrees=90)
+        return derivatives.position + inwards*depth
+    
+    return derivatives.position
+  
+  return [control_point (distance) for distance in subdivisions (0, wall_curve_length, max_length = 2)]
+
+
+preview (controller_board, sensor_boards, power_wire, ground_wire, signal_wires, wall_curve, BSplineCurve (perforated_control_points (0), BSplineDimension (periodic = True)))
 
