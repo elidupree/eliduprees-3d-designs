6e2173212571b4f31242136c00383d0856179b16
elidupree <vcs@elidupree.com>
2023-09-03 18:47:31 -0400

    designed bending brake diagonal clamps

--- metamaterials.py
+++ metamaterials.py
@@ -4,6 +4,8 @@ from pyocct_system import *
 
 initialize_pyocct_system()
 
+inch = 25.4
+
 @run_if_changed
 def deforming_slot():
     zigs = 10
@@ -84,3 +86,65 @@ def single_fold_deforming_slot():
     result = solid.cut(slot)
     save_STL("single_fold_deforming_slot", result)
     preview(result)
+
+@run_if_changed
+def bending_brake_diagonal_clamp():
+    presser_plate_thickness = 11
+    clamp_width = 10
+    clamp_thickness = 10
+    bolt_radius = 3
+    spring_space = 50
+
+
+    a = Vector(0, 52, 32)
+    presser_plate_length = a.magnitude() + 2
+    slot_length = presser_plate_length + 5
+    clamp_length = presser_plate_length + spring_space
+    da = Direction (a)
+    db = (Direction (a) @ Rotate(Right, Degrees(90)))
+    b = db * presser_plate_thickness
+    presser_plate_ish = Vertex (Origin).extrude (da*presser_plate_length).extrude (b).extrude (Left*2*inch)
+
+    clamp_cross_section = Face(Wire([
+        Origin,
+        Origin + Back*da[1]*(slot_length+clamp_thickness),
+        Origin + da*(clamp_length),
+        Origin + da*(clamp_length) + db*(presser_plate_thickness),
+        Origin + da*(slot_length+clamp_thickness) + db*(presser_plate_thickness+clamp_thickness),
+        Origin + db*(presser_plate_thickness+clamp_thickness),
+    ], loop = True)).cut(Vertex (Origin).extrude (da*slot_length).extrude (b)).cut(HalfSpace(Origin, Front))
+    clamp = clamp_cross_section.extrude(Left*clamp_width)
+    clamp = clamp.cut(Face (Circle (Axes (Origin + db*presser_plate_thickness/2 + Left*clamp_width/2, da), bolt_radius)).extrude(da * 1000))
+
+    spring_peg = Face(Wire([
+        Origin,
+        Point(-10, 0, 0),
+        Point(-10, 5/2, 0),
+        Point(-6, 4/2, 0),
+        Point(0, 6, 0),
+    ], loop = True)).revolve(Left) @ Translate(da*(clamp_length - 6) + db*presser_plate_thickness/2 + Left*clamp_width)
+    clamp = Compound(clamp, spring_peg)
+
+    spring_grabber = Face(Wire([
+        Point(-5, 0, 0),
+        Point(-5, 3, 0),
+        Point(-4, 3, 0),
+        Point(0, 4/2, 0),
+        Point(4, 3, 0),
+        Point(5, 3, 0),
+        Point(5, 0, 0),
+    ], loop = True)).revolve(Left)
+    s = Vertex(Origin).extrude(Back*3, Front * 25).extrude(Up*(presser_plate_thickness + 6), centered=True)
+    s = s.cut(Vertex(Origin).extrude(Front*(4/2 + 3 + 2), Front * 1000).extrude(Up*(presser_plate_thickness), centered=True))
+    s = s.extrude(Left*10, centered=True)
+    a = Direction(0,1,1)
+    b = Direction(0,1,-1)
+    s = s.cut(HalfSpace(Origin + a*3, a)).cut(HalfSpace(Origin + b*3, b)).cut(HalfSpace(Origin + Back*3/math.sqrt(2), Back))
+    s = s.cut(Vertex(Origin).extrude(Back*100, Front * (4/2 + 2.3)).extrude(Up*100, centered=True).extrude(Left*8, centered=True))
+    spring_grabber = Compound(spring_grabber, s)
+
+    save_STL("bending_brake_diagonal_clamp", clamp @ Rotate(Back, Degrees(90)))
+
+    preview (clamp, presser_plate_ish, spring_grabber @ Transform(Right,da,db) @ Translate(da*(presser_plate_length + 4/2 + 3 + 2) + db*presser_plate_thickness/2 + Left*(clamp_width + 6)))
+
+
