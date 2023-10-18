f34c3df4a825e02cb327a5489abaded21f6b08e3
elidupree <vcs@elidupree.com>
2023-10-18 19:08:21 -0400

    made monitor diptych mount, other work

---
+++ monitor_diptych_mount.py
@@ -0,0 +1,68 @@
+import math
+from pyocct_system import *
+
+initialize_pyocct_system()
+
+inch = 25.4
+lots = inch*50
+
+monitor_thickness = 9.35
+monitor_width = 11.7*inch
+monitor_height = 7.8*inch
+
+monitor_distance = 22* inch
+
+rod_diameter = inch/4
+rod_radius = rod_diameter/2
+
+#monitor_angle =
+
+bracket_width = 60
+bracket_inset = (monitor_width - 180)/2
+bracket_wall_thickness = 3
+bezel_coverage = 2
+
+plate_clearance = 13 - rod_diameter
+
+sheath_radius = rod_radius + bracket_wall_thickness
+
+@run_if_changed
+def bracket_shared():
+    sheath = Face (Circle(Axes(Origin, Up), sheath_radius)).extrude (Down * lots, centered=True)
+    wing = Vertex(Origin).extrude(Back*sheath_radius, Front*(rod_radius+plate_clearance+monitor_thickness+bracket_wall_thickness)).extrude (Down * lots, centered=True).extrude (Right * lots).cut(HalfSpace(Origin, Direction(-1, -1, 0)))
+    filter = Face(Wire([
+        Origin + Down * bracket_inset + Left * sheath_radius,
+        Origin + Down * bracket_inset + Right * sheath_radius,
+        Origin + Right * bracket_width + Up * bracket_wall_thickness,
+        Origin + Left * sheath_radius + Up * bracket_wall_thickness,
+    ], loop=True)).extrude (Back*lots, centered=True)
+    monitor_cut = Compound(
+        Vertex(Origin + Front*(rod_radius+plate_clearance)).extrude(Front*monitor_thickness).extrude (Down * lots).extrude (Right * lots, centered=True),
+        Vertex(Origin + Front*sheath_radius + Down*bezel_coverage).extrude(Front*lots).extrude (Down * lots).extrude (Right * lots, centered=True),
+    )
+    rod_cut = Face (Circle(Axes(Origin, Up), rod_radius)).extrude (Down * lots, centered=True)
+    return Compound(sheath, wing).intersection(filter).cut(monitor_cut).cut(rod_cut)
+
+def bracket_cuts(parity):
+    f = Face(Circle(Axes(Origin, Up), sheath_radius + 0.4))
+    cuts = [
+        f.extrude(Up*(top+0.1), Up*(bottom-0.1))
+        for i, (top, bottom) in enumerate(pairs(subdivisions(bracket_wall_thickness, -bracket_inset, amount=7)))
+        if i % 2 == parity
+    ]
+    return Compound(cuts)
+
+@run_if_changed
+def bracket_right():
+    return bracket_shared.cut(bracket_cuts(0))
+
+@run_if_changed
+def bracket_left():
+    return (bracket_shared @ Mirror(Left)).cut(bracket_cuts(1))
+
+@run_if_changed
+def save():
+    save_STL("bracket_left", bracket_left)
+    save_STL("bracket_right", bracket_right)
+
+preview(bracket_right, bracket_left)
--- woodstove_automation.py
+++ woodstove_automation.py
@@ -17,20 +17,25 @@ motor_ring_depth = 1.9
 
 rod_radius = 9.5/2
 min_exposed_rod_length = 20
-max_knob_radius = 12
+max_knob_radius = 21.4/2 #12
 knob_length=35
 dial_radius=33/2
 dial_center = Origin+Right*(dial_radius+14) + Up*3
 dial_depth=24
 plate_descent = 30
 
-roller_smallest_radius = 5
-roller_largest_radius = 7
+roller_solid_radius = 11/2
+roller_smallest_radius = roller_solid_radius + 0.5
+roller_largest_radius = roller_solid_radius + 1.5
 gear_thickness =4
 teeth1 = 30
 teeth2 = 13
 
+bearing_od = 12
+bearing_depth = 4
+
 pitch_radius = roller_smallest_radius+rod_radius
+print("pitch_radius:", pitch_radius)
 
 @run_if_changed
 def first_gear_info():
@@ -50,7 +55,7 @@ def motor():
     body = Vertex (center).extrude (Left *motor_depth).extrude (Front * motor_width, centered = True).extrude (Up * motor_width, centered = True)
     ring =Face (Circle (Axes (center, Right),motor_ring_radius)).extrude (Right *motor_ring_depth)
     shaft =Face (Circle (Axes (center, Right),motor_shaft_radius)).extrude (Right *motor_shaft_length).cut (Vertex (center + Right *(motor_shaft_length - motor_d_length)+ Down*(motor_shaft_radius - motor_d_diameter)).extrude (Right *lots).extrude (Up * lots).extrude (Back*lots, centered = True))
-    return Compound (body, ring, shaft)
+    return [body, ring, shaft]
 
 @run_if_changed
 def dial():
@@ -73,6 +78,24 @@ def gears():
     second = first @ Translate(Down*pitch_radius*2)
     third = (drive_gear_info.shape @ Rotate(Back, Degrees(90))) @ Translate(motor_shaft_tip_center - Origin)
     third = third.extrude(Left *gear_thickness)
+    third = third.cut(motor[2])
+    save_STL("drive_gear", third)
+    #third = Chamfer(third, [(e,  for e in third.edges()])
     return [first, second, third]
 
-preview (plate, rod, motor, dial, gears)
+@run_if_changed
+def rollers():
+    first = Face(Circle(Axes(Origin, Left), roller_largest_radius)).extrude(Left*(max_knob_radius+1), Right*(rod_radius+1)) @ Translate(Back*roller_center_y + Up*pitch_radius)
+    second = first @ Translate(Down*pitch_radius*2)
+    return [first, second]
+
+@run_if_changed
+def bearings():
+    first = Face(Circle(Axes(Origin, Left), bearing_od/2)).extrude(Left*(bearing_depth)) @ Translate(Back*roller_center_y + Up*pitch_radius + Left*(max_knob_radius+1+gear_thickness))
+    second = first @ Translate(Down*pitch_radius*2)
+    result = [first, second]
+    result.extend([b @ Translate(Right*(max_knob_radius+1+gear_thickness+rod_radius+1 + bearing_depth)) for b in result])
+    return result
+
+
+preview (plate, rod, motor, dial, gears, rollers, bearings)
