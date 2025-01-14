52b3e72a71a438fc83e7f5a92c2e14c71c18b6ab
elidupree <vcs@elidupree.com>
2025-01-14 06:10:53 -0500

    started adding stretch to unroll, but didn't get very far

--- unroll.py
+++ unroll.py
@@ -1,35 +1,6 @@
 
 from pyocct_system import *
 
-def assert_same_length(a, b, c, d):
-  l0 = (a - b).length()
-  l1 = (c - d).length()
-  assert abs(l0 - l1) < 0.00001
-
-def unrolled_next_triangle_point(prev_a, prev_b, unrolled_prev_a, unrolled_prev_b, new_point):
-  # calculate relative position of the new point in
-  # two dimensions _relative_ to the current section,
-  # these dimensions being crosswise (c) and lengthwise (l).
-  # Crosswise movement is simple and reflects the 3D movement, signedly;
-  # but for the l dimension, _any_ non-crosswise movement in 3D
-  # becomes positive lengthwise movement in 2D.
-  cdir = Direction(prev_a, prev_b)
-  unrolled_cdir = Direction(unrolled_prev_a, unrolled_prev_b)
-  unrolled_ldir = unrolled_cdir2 @ Rotate(Up, Turns(1/4))
-
-  # use prev_a / unrolled_prev_a as a reference point; this would yield
-  # the same result if we picked prev_b / unrolled_prev_b,
-  # since they differ only in c, which cancels
-  d = new_point - prev_a
-  c = d.dot(cdir)
-  l = d.projected_perpendicular(cdir).length()
-  result = unrolled_prev_a + unrolled_cdir * c + unrolled_ldir * l
-
-  # error if I made a mistake:
-  assert_same_length(prev_a, new_point, unrolled_prev_a, result)
-  assert_same_length(prev_b, new_point, unrolled_prev_b, result)
-  return result
-
 
 # o = original, u = unrolled
 class UnrolledPoint:
@@ -44,13 +15,16 @@ class TriangleCandidate:
     self.was_ccw = was_ccw
 
 class UnrolledEdge:
-  def __init__(self, a, b):
+  def __init__(self, a, b, stretch = 1):
     assert isinstance(a, UnrolledPoint)
     assert isinstance(b, UnrolledPoint)
     assert (a.o - b.o).length() > 0.000001
     self.a = a
     self.b = b
-    assert_same_length(a.o, b.o, a.u, b.u)
+    self.stretch = stretch
+    self.lo = (a.o - b.o).length()
+    self.lu = (a.u - b.u).length()
+    assert abs(self.lo*stretch - self.lu) < 0.00001
     self.along_edge_o = Direction(self.a.o, self.b.o)
     self.along_edge_u = Direction(self.a.u, self.b.u)
     # by convention, the interior of a curve is on the ccw side, so outside is always cw
