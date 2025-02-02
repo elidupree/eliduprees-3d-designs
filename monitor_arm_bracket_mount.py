33b0a8479ff7a4f0063721aa34e5bb2ecc8cf441
elidupree <vcs@elidupree.com>
2025-02-02 16:32:11 -0500

    improved monitor arm bracket mount

--- monitor_arm_bracket_mount.py
+++ monitor_arm_bracket_mount.py
@@ -19,8 +19,10 @@ measured_grip_width = 36
 strong_wall_thickness = 2.0
 
 plate_side_leeway = 1.0
-plate_thickness_leeway = 0.3
-leeway_plate_thickness = measured_plate_thickness + plate_thickness_leeway
+plate_thickness_leeway = 1.0
+leewayed_plate_height = measured_plate_height + plate_thickness_leeway/2
+leewayed_plate_thickness = measured_plate_thickness + plate_thickness_leeway
+leewayed_security_screw_hole_height = measured_security_screw_hole_height + 1
 block_width = measured_plate_width + strong_wall_thickness*2 + plate_side_leeway
 foot_y = block_width/2 - 8
 
@@ -31,18 +33,23 @@ foot_screw_radius = 2
 foot_screw_leeway = 0.3
 security_screw_leeway = 0.3
 
+plateish_corner = Point(50, 0, 10)
+VESAish_corner = Point(10, 0, leewayed_security_screw_hole_height+strong_wall_thickness)
+security_screw_plate_bottom_corner = Point(-measured_security_screw_hole_backset - security_screw_radius - security_screw_leeway - strong_wall_thickness, 0, leewayed_security_screw_hole_height)
+
 @run_if_changed
 def bracket_mount():
 
     big_block = Face(Wire([
         # Point(0, 0, 0),
         Point(50, 0, 0),
-        Point(50, 0, 10),
-        Point(10, 0, measured_security_screw_hole_height+strong_wall_thickness),
-        Point(-measured_security_screw_hole_backset - security_screw_radius - security_screw_leeway - strong_wall_thickness, 0, measured_security_screw_hole_height+strong_wall_thickness),
-        Point(-measured_security_screw_hole_backset - security_screw_radius - security_screw_leeway - strong_wall_thickness, 0, measured_security_screw_hole_height),
-        Point(-leeway_plate_thickness-strong_wall_thickness, 0, measured_security_screw_hole_height),
-        Point(-leeway_plate_thickness-strong_wall_thickness, 0, 0),
+        plateish_corner,
+        VESAish_corner,
+        Point(-measured_security_screw_hole_backset - security_screw_radius - security_screw_leeway - strong_wall_thickness, 0, leewayed_security_screw_hole_height + strong_wall_thickness),
+        security_screw_plate_bottom_corner,
+        # Point(-leewayed_plate_thickness-strong_wall_thickness, 0, leewayed_security_screw_hole_height),
+        security_screw_plate_bottom_corner.projected(onto=Plane(Point(-leewayed_plate_thickness-strong_wall_thickness-1, 0, 0), Right), by=Direction(plateish_corner, VESAish_corner)),
+        Point(-leewayed_plate_thickness-strong_wall_thickness-1, 0, 0),
     ], loop=True)).extrude(Back*block_width, centered=True)
 
     foot_cut = Compound(
@@ -53,10 +60,15 @@ def bracket_mount():
 
     security_screw_hole = Face(Circle(Axes(Point(-measured_security_screw_hole_backset, 0, 0),Up), security_screw_radius + security_screw_leeway)).extrude(Up*100)
 
+    extra_diagonals = Face(Wire([
+        security_screw_plate_bottom_corner,
+        security_screw_plate_bottom_corner.projected(Plane(Origin, Right), by=Direction(plateish_corner, VESAish_corner))
+    ], loop=True))
+
     result = big_block.cut([
-        Vertex(Origin).extrude(Left*leeway_plate_thickness).extrude(Back*(measured_plate_width+plate_thickness_leeway), centered=True).extrude(Up*measured_plate_height),
-        Vertex(Origin).extrude(Left*10).extrude(Back*measured_grip_width, centered=True).extrude(Up*measured_plate_height),
-        Vertex(Origin).extrude(Left*10).extrude(Back*measured_nub_width, centered=True).extrude(Up*measured_nub_height),
+        Vertex(Origin).extrude(Left*leewayed_plate_thickness).extrude(Back*(measured_plate_width+plate_thickness_leeway), centered=True).extrude(Up*leewayed_plate_height),
+        Vertex(Origin).extrude(Left*100).extrude(Back*measured_grip_width, centered=True).extrude(Up*leewayed_security_screw_hole_height),
+        Vertex(Origin).extrude(Left*100).extrude(Back*measured_nub_width, centered=True).extrude(Up*measured_nub_height),
         security_screw_hole,
     ])
     # preview(result)
@@ -64,7 +76,7 @@ def bracket_mount():
 
     result = result.cut([foot_cut @ Translate(x,y,0) for x in foot_xs for y in foot_ys])
     save_STL("bracket", result)
-    export("bracket.stl", "bracket_2.stl")
+    export("bracket.stl", "bracket_3.stl")
     return result
 
 
