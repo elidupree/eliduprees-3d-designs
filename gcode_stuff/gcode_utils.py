a8a239e8a7a8f9452634db49c5a7425ba2a62ec7
elidupree <vcs@elidupree.com>
2025-03-01 07:08:01 -0500

    Improved the start/finish gcode, partly using my custom Cura plugin

--- gcode_stuff/gcode_utils.py
+++ gcode_stuff/gcode_utils.py
@@ -2,45 +2,54 @@ import math
 
 filament_diameter = 1.75
 mm3_per_extrusion_distance = filament_diameter * filament_diameter
+ender3_size_y = 235
 ender3_center_x = 235/2
-ender3_center_y = 235/2
+ender3_center_y = ender3_size_y/2
 
-def start_gcode(min_temp = 200, good_temp = 255):
+def start_gcode(min_temp = 240, good_temp = 255):
     return f'''
 G90     ; Use absolute positioning
 G92 E0  ; Set extruder reference point
 M107    ; Fan off to start
 
 ; start heating while we move
-M104 S{min_temp}
+M104 S{good_temp}
 
 G28     ; Home all axes
 
-; need to reach min temp before initial wipe:
-; M109 S{min_temp}
-; ...but keep heating up while we do it:
+; need to reach min temp to make sure we don't try to shove cold filament into the build plate, but keep heating up while we do it
+; (relying on EliDupree custom behavior for M109+M104)
+M109 S{min_temp}
+M104 S{good_temp}
+  
+G1 X0.1 Y10 Z0 F2000.0 ; enter the bed on Z=zero, to scrape off any current ooze
+  
+; get closer to target temp before initial wipe, but keep heating up while we do it
+; (relying on EliDupree custom behavior for M109+M104)
+M109 S{max(min_temp, good_temp - 10)}
+M104 S{good_temp} ; ...
+
+; start initial wipe, modified from Cura's:
+; G1 Z2.0 F3000 ; Move Z Axis up little to prevent scratching of Heat Bed
+G1 X0.1 Y20 Z0.3 F5000.0 ; Move to start position
+G1 X0.1 Y200.0 Z0.3 F1500.0 E15 ; Draw the first line
+G1 X1.3 Y200.0 Z0.3 F5000.0 ; Move to side a little
+
+; Get near max temp before we finish the initial wipe
+; (was relying on EliDupree custom behavior for M109+M104, but actually we probably better get to the target for real, to make it steady for the real print)
+; M109 S{max(min_temp, good_temp-5)}
 ; M104 S{good_temp}
 M109 S{good_temp}
 
-; start initial wipe, borrowed from Cura:
-G1 Z2.0 F3000 ; Move Z Axis up little to prevent scratching of Heat Bed
-G1 X0.1 Y20 Z0.3 F5000.0 ; Move to start position
-G1 X0.1 Y200.0 Z0.3 F1500.0 E15 ; Draw the first line
-G1 X0.4 Y200.0 Z0.3 F5000.0 ; Move to side a little
-
-; ...want to reach max temp before we finish the initial wipe:
-; M109 S{good_temp}
-
-; rest of initial wipe, borrowed from Cura:
-G1 X0.4 Y20 Z0.3 F1500.0 E30 ; Draw the second line
+; rest of initial wipe, modified from Cura's:
+G1 X1.3 Y20 Z0.3 F1500.0 E30 ; Draw the second line
+G1 X5 Y20 Z0.1 F5000.0 ; Move over and get lower, to make sure not to drag the filament with us
 G92 E0 ; Reset Extruder
-G1 Z2.0 F3000 ; Move Z Axis up little to prevent scratching of Heat Bed
-G1 X5 Y20 Z0.3 F5000.0 ; Move over to prevent blob squish
 
 '''
 
 def finish_gcode():
-    machine_depth = 235
+    machine_depth = ender3_size_y
     return f'''
 ; borrowed from Cura, just tweaked order (we can turn heat off before leaving):
 M104 S0 ;Turn-off hotend
@@ -49,11 +58,18 @@ G91 ;Relative positioning
 G1 E-2 F2700 ;Retract a bit
 G1 E-2 Z0.2 F2400 ;Retract and raise Z
 G1 X5 Y5 F3000 ;Wipe out
-M106 S0 ;Turn-off fan
-G1 Z10 ;Raise Z more
+G1 Z10 E-10 ;Raise Z more, retract more
 G90 ;Absolute positioning
 
 G1 X0 Y{machine_depth} ;Present print
+M106 S0 ;Turn-off fan
+; G1 Z2 ;Go to bed to help suppress oozing. note that the back 17mm of Y are "safe" from smacking the X rail into the print (the nozzle can't print that far back). (I ended up not using this because I don't want to risk descending into a print if it got stuck to the hotend)
+; M106 S0 ;Turn-off fan
+; G1 X10 Z0.2 F5000 ;wipe off any current ooze, first quickly...
+; G91 ;Relative positioning
+; G1 Y-10 Z-0.2 F5000 ; then down to bed to block harder
+; G90 ;Absolute positioning
+; G1 X0 F10 ; ...then very slowly
 
 M84 X Y E ;Disable all steppers but Z
     '''
