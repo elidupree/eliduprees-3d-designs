c668d76256a6b0da7017b53982a3ce46c2c18bf1
elidupree <vcs@elidupree.com>
2025-08-04 07:31:33 -0400

    Serialization work from yesterday

--- data_model/serialization.py
+++ data_model/serialization.py
@@ -4,22 +4,26 @@ Our serialization format is literally just de-duplicated JSON, because anything 
 
 Serialized-objects that contain deduplicated objects represent the references as 21 base64 characters (126 bits).
 
-Any object has 2 serializable forms: "serializable-value", which is the whole JSON form of "the object but all subobjects are replaced with serializable-usages"; and "serializable-usage", which is either
-* if the whole JSON form of the object is smaller than that of a reference, the whole JSON form;
-* otherwise, a string that's 21 base64 characters of the siphash128 of the serializable-value.
+Any object has 2 serializable forms: "serializable-value", which is the whole JSON-value of "the object but all subobjects are replaced with serializable-usages"; and "serializable-usage", which is either
+* if the whole JSON-*string* of the object is smaller than that of a reference, the whole JSON-*value* of it;
+* otherwise, a *string* that's 21 base64 characters of the siphash128 of the serializable-value.
 
 Thus, when you're deserializing a container, you don't need a special marker to tell whether an inner string is a reference or not - you can just check how big it is.
 
+We do extend JSON a little to allow arbitrary objects as keys, by using their serialized-usage as the key. (The client could technically write the strings there themselves, but this system needs to take responsibility for storing the referenced objects.) We also make objects __hash__ as their serialized-usage.
+
 Clearly, we don't need deduplicated-storage for things smaller than a reference. For things as large as a reference, we store them in a SQLite database, which is primarily a mapping from references to values.
 
-The database also includes a reference count for each stored object. The database also contains mutable cells that map one value to another - typically used as a cache mapping a thunk to its output. These "mutable cell" values are the sole persistent objects that won't be garbage-collected, and hold entries in reference-counts. This file is not responsible for defining conventions around what you use the mutable cells for; rather, this file merely provides an interface to a database, and representations of the stored objects.
+The database primarily acts as a key-value store, and also contains all subobjects of existing keys or values, with a reference count for each subobject. This file is not responsible for defining conventions around what you use the key-value store for; rather, this file merely provides an interface to a database, and representations of the stored objects.
 
 This file's responsibilities include:
- * Defining classes for serialization and for the JSON container typesresponsibility is to enforce per-key read-write locking of the mutable cells: you can't change or delete one while any process is holding a reference to it. (It's intended for users to load values lazily, so
+ * Defining classes for serialization and for the (immutable) JSON container types
+ * Defining database transactions that are guaranteed to leave the database with valid reference counts
 
 """
 import json
 import os
+import re
 import sqlite3
 import typing
 from pathlib import Path
@@ -36,12 +40,13 @@ def _database_connection():
         _stored_database_connection = sqlite3.connect(_database_file)
 
 class Serializable:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self._cached_serializable_usage = None
         self._cached_serializable_value = None
         self._cached_serialized_value = None
         self._cached_serialized_usage = None
         self._saved = False
+        super().__init__(*args, **kwargs)
 
     def _serializable_value(self):
         return self
@@ -53,16 +58,18 @@ class Serializable:
 
     def serialized_value(self):
         if self._cached_serialized_value is None:
-            self._cached_serialized_value = self._serializable_value()
-        return self._serializable_value()
+            self._cached_serialized_value = json.dumps(self.serializable_value())
+        return self._cached_serialized_value
+
+    def needs_indirection(self):
+        return len(self.serialized_value()) >= 21+2
 
     def serializable_usage(self):
         if self._cached_serializable_usage is None:
-            sv = self.serialized_value()
-            if len(sv) < 21+2:
-                self._cached_serializable_usage = self.serializable_value()
+            if self.needs_indirection(self):
+                self._cached_serializable_usage = b64encode(siphash_128(key=siphash_key, data=self.serialized_value().encode('ascii')))[:21]
             else:
-                self._cached_serializable_usage = b64encode(siphash_128(key=siphash_key, data=sv.encode('ascii')))[:21]
+                self._cached_serializable_usage = self.serializable_value()
         return self._cached_serializable_usage
 
     def serialized_usage(self):
@@ -77,10 +84,161 @@ class Serializable:
         if self._saved is False and self not in objects:
             objects.add(self)
 
+    def __hash__(self):
+        return hash(self.serialized_usage())
 
-def remove_global_persistent_map_entry(key: Serializable):
 
-def save_global_persistent_map_entry(key: Serializable, value: Serializable):
+
+def forbidden_mutating_operation(*args, **kwargs):
+    raise RuntimeError("Mutating an immutable (Serializable) collection")
+
+
+class SerializableStr(Serializable, str):
+    pass
+
+
+class SerializableList(Serializable, list):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for i,e in enumerate(self):
+            self[i] = make_serializable(e)
+    # def __init__(self, items: typing.List[Serializable]):
+    #     super().__init__(self)
+    #     self.items = items
+
+    def _serializable_value(self):
+        return [x.serializable_usage() for x in self]
+
+    def serializable_subobjects(self):
+        return iter(self)
+
+for method in re.finditer(r"[^, ]+", "__setattr__, __delattr__, __setitem__, __delitem__, __iadd__, __imul__, append, extend, insert, remove, pop, clear, sort, reverse"):
+    setattr(SerializableList, method, forbidden_mutating_operation)
+
+
+class SerializableDict(Serializable, dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        items = list(self.items())
+        self.clear()
+        for k,v in items:
+            self[make_serializable(k)] = make_serializable(v)
+
+    def _serializable_value(self):
+        return {k.serialized_usage(): v.serializable_usage() for k,v in self.items()}
+
+    def serializable_subobjects(self):
+        yield from self.keys()
+        yield from self.values()
+
+for method in re.finditer(r"[^, ]+", "__setattr__, __delattr__, __setitem__, __delitem__, __ior__, clear, pop, popitem, setdefault"):
+    setattr(SerializableDict, method, forbidden_mutating_operation)
+
+
+_object_cache = {}
+
+def make_serializable(x):
+    if type(x) in [type(None), bool, int, float] or isinstance(x, Serializable):
+        s = x
+    elif type(x) is str:
+        s = SerializableStr(x)
+    elif type(x) is list:
+        s = SerializableList(x)
+    elif type(x) is dict:
+        s = SerializableDict(x)
+    else:
+        raise RuntimeError("tried to make unrecognized type serializable")
+
+    if isinstance(s, Serializable):
+        s = _object_cache.setdefault(s.serialized_usage(), s)
+
+    return s
+
+
+def _serialized_usage(x):
+    if isinstance(x, Serializable):
+        return x.serialized_usage()
+    else:
+        return json.dumps(x)
+
+def _needs_indirection(x):
+    return isinstance(x, Serializable) and x.needs_indirection()
+
+
+def _store_referents_of_usage(cursor: sqlite3.Cursor, x: Serializable):
+    if _needs_indirection(x):
+        _store_value(x)
+
+def _store_value(cursor: sqlite3.Cursor, x: Serializable):
+    if todo_database_store(x.serializable_usage(), x):
+        for y in x.serializable_subobjects():
+            if _needs_indirection(y):
+                _store_value(cursor, y)
+
+
+def _load_referent(cursor: sqlite3.Cursor, reference):
+    query_result = TODO
+    return _load_value(cursor, json.loads(query_result))
+
+def _load_usage(cursor: sqlite3.Cursor, x):
+    if type(x) is str and len(x) >= 21:
+        return _load_referent(cursor, x)
+    else:
+        return x
+
+def _load_value(cursor: sqlite3.Cursor, x):
+    if type(x) is list:
+        x = [_load_usage(cursor, e) for e in x]
+    if type(x) is dict:
+        x = {_load_usage(cursor, json.loads(k)): _load_usage(cursor, v)} for k,v in x.items()}
+    return make_serializable(x)
+
+
+def _remove_referent(cursor: sqlite3.Cursor, reference):
+    if query_result = todo_database_remove(reference):
+        _remove_value(cursor, json.loads(query_result))
+
+def _remove_referents_of_usage(cursor: sqlite3.Cursor, x):
+    if type(x) is str and len(x) >= 21:
+        _remove_referent(cursor, x)
+
+def _remove_value(cursor: sqlite3.Cursor, x):
+    if type(x) is list:
+        for e in x:
+            _remove_referents_of_usage(cursor, e)
+    if type(x) is dict:
+        for k, v in x.items():
+            _remove_referents_of_usage(cursor, json.loads(k))
+            _remove_referents_of_usage(cursor, v)
+
+
+def until_not_rolled_back(operation):
+    for _ in range(100):
+        try:
+            cursor = _database_connection().cursor(isolation_level="DEFERRED")
+            operation(cursor)
+            cursor.commit()
+        except sqlite3.OperationalError as e:
+            if e.sqlite_errorname != "SQLITE_CONSTRAINT":
+                raise
+    raise RuntimeError("DB op failed 100 times, there's probably a bug")
+
+def _remove_key_optimistic(cursor: sqlite3.Cursor, key):
+    value_usage = todo_database_remove(_serialized_usage(key))
+    _remove_referents_of_usage(json.loads(value_usage))
+
+def _remove_key(key):
+    until_not_rolled_back(lambda cursor: _remove_key_optimistic(cursor, key))
+
+def _set_key_value_optimistic(cursor: sqlite3.Cursor, key, value):
+    # store the key-value entry itself, _serialized_usage(key) _serialized_usage(value)
+    _store_referents_of_usage(cursor, key)
+    _store_referents_of_usage(cursor, value)
+
+
+def _set_key_value(key, value):
+    until_not_rolled_back(lambda cursor: _set_key_value_optimistic(cursor, key, value)
+
     remove_global_persistent_map_entry(key)
     cursor = _database_connection().cursor(isolation_level="DEFERRED")
     new_saved_objects_by_id = {}
@@ -99,34 +257,3 @@ def save_global_persistent_map_entry(key: Serializable, value: Serializable):
     cursor.execute("INSERT INTO objects_by_id VALUES (?, ?)", new_saved_objects_by_id.items())
     cursor.execute("INSERT INTO parentages (parent, child) VALUES (?, ?)", new_saved_parentages)
     cursor.commit()
-
-
-
-
-class SerializableList(Serializable, list):
-    # def __init__(self, items: typing.List[Serializable]):
-    #     super().__init__(self)
-    #     self.items = items
-
-    def _serializable_value(self):
-        return [x.serializable_usage() for x in self]
-
-
-
-def serializable_value(x):
-    if type(x) is list:
-        return [serializable_usage(y) for y in x]
-    if type(x) is dict:
-        return {serializable_usage(k): serializable_usage(v) for k,v in x.items()}
-    return x
-
-siphash_key = bytes.fromhex("78ca81054e1df045447cd7d48958de7d")
-def hashid_of_serializable_value(x):
-    return siphash_128(key=siphash_key, data=json.dumps(x))
-
-def serializable_usage(x):
-    use_
-    # don't json.dumps giant objects; hardcode the fact that
-    if (type(x) is list or type(x) is dict):
-        return {serializable_usage(k): serializable_usage(v) for k,v in x.items()}
-    jsonstr = json.dumps(x)
