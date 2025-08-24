399a06bcd0fa5e8b17dd6c51dcdc167352a6bf3d
elidupree <vcs@elidupree.com>
2025-08-24 07:35:52 -0400

    more work on Rust side, switched to bencode value type from library

--- eliduprees_3d_designs_storage_api/src/lib.rs
+++ eliduprees_3d_designs_storage_api/src/lib.rs
@@ -1,85 +1,52 @@
-extern crate bendy;
+extern crate const_hex;
 
 use bendy::decoding::FromBencode;
-use bendy::encoding::{SingleItemEncoder, ToBencode};
+use bendy::encoding::ToBencode;
+use std::borrow::Cow;
+use std::cmp::Ordering;
 use std::collections::BTreeMap;
+use std::hash::{Hash, Hasher};
 use std::sync::Arc;
 
 pub const HASH_ID_BYTE_LENGTH: usize = 16;
 pub const HASH_ID_BENCODE_BYTE_LENGTH: usize = HASH_ID_BYTE_LENGTH + "16:".len();
 pub const SIPHASH_KEY: [u8; 16] =
-    const_hex::const_decode_to_array(b"78ca81054e1df045447cd7d48958de7d").unwrap();
+    match const_hex::const_decode_to_array(b"78ca81054e1df045447cd7d48958de7d") {
+        Ok(b) => b,
+        Err(_) => unreachable!(),
+    };
 
 pub type ByteString = Vec<u8>;
-pub enum BencodeValue {
-    ByteString(ByteString),
-    List(Vec<BencodeValue>),
-    Map(BTreeMap<ByteString, BencodeValue>),
-}
+pub type BencodeValue = bendy::value::Value<'static>;
 
+#[derive(Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
 pub struct SerializedBencode {
     pub data: ByteString,
 }
 
 enum CaReferents {
     ByteString,
-    List(Vec<ArcIntern<Ca>>),
-    Map(BTreeMap<ArcIntern<Ca>, ArcIntern<Ca>>),
+    List(Vec<Arc<Ca>>),
+    Map(BTreeMap<Arc<Ca>, Arc<Ca>>),
 }
 
 struct CaCaches {
     raw_form: BencodeValue,
     id_raw: BencodeValue,
     serialized: SerializedBencode,
-    id_serialized: ArrayVec<u8, HASH_ID_BENCODE_BYTE_LENGTH>,
+    id_serialized: SerializedBencode,
 }
 pub struct Ca {
     caches: CaCaches,
     referents: CaReferents,
 }
 
-impl ToBencode for BencodeValue {
-    const MAX_DEPTH: usize = 2048;
-
-    fn encode(&self, encoder: SingleItemEncoder) -> Result<(), bendy::encoding::Error> {
-        match self {
-            BencodeValue::ByteString(bytes) => encoder.emit(&bendy::encoding::AsString(bytes)),
-            BencodeValue::List(list) => encoder.emit(list),
-            BencodeValue::Map(map) => encoder.emit(map),
-        }
-    }
-}
-
-impl FromBencode for BencodeValue {
-    fn decode_bencode_object(
-        object: bendy::decoding::Object,
-    ) -> Result<Self, bendy::decoding::Error>
-    where
-        Self: Sized,
-    {
-        use bendy::decoding::Object::*;
-        match &object {
-            List(_) => Ok(BencodeValue::List(Vec::<_>::decode_bencode_object(object)?)),
-            Dict(_) => Ok(BencodeValue::Map(BTreeMap::<_, _>::decode_bencode_object(
-                object,
-            )?)),
-            Integer(_) => bendy::decoding::Error::unexpected_token(
-                "an EliDuprees-3D-designs-value",
-                "a bencode integer",
-            ),
-            Bytes(bytes) => Ok(BencodeValue::ByteString(bytes.to_vec())),
-        }
-    }
-}
-
-impl BencodeValue {
-    pub fn serialize(&self) -> SerializedBencode {
-        SerializedBencode {
-            data: self.to_bencode().unwrap(),
-        }
-    }
-}
 impl SerializedBencode {
+    pub fn from_bencode_value(value: &BencodeValue) -> SerializedBencode {
+        SerializedBencode {
+            data: value.to_bencode().unwrap(),
+        }
+    }
     pub fn deserialize(&self) -> BencodeValue {
         BencodeValue::from_bencode(&self.data).unwrap()
     }
@@ -93,13 +60,13 @@ impl SerializedBencode {
 }
 impl CaCaches {
     pub fn from_raw(raw_form: BencodeValue) -> CaCaches {
-        let serialized = raw_form.serialize();
+        let serialized = SerializedBencode::from_bencode_value(&raw_form);
         let id_raw = if serialized.needs_indirection() {
-            BencodeValue::ByteString(serialized.canonical_hash().to_vec())
+            BencodeValue::Bytes(Cow::Owned(serialized.canonical_hash().to_vec()))
         } else {
             raw_form.clone()
         };
-        let id_serialized = id_raw.serialize();
+        let id_serialized = SerializedBencode::from_bencode_value(&id_raw);
         CaCaches {
             raw_form,
             id_raw,
@@ -110,30 +77,30 @@ impl CaCaches {
 }
 impl Ca {
     pub fn needs_indirection(&self) -> bool {
-        self.serialized.needs_indirection()
+        self.serialized().needs_indirection()
     }
-    pub fn new_byte_string(bytes: ByteString) -> ArcIntern<Ca> {
-        ArcIntern::new(Ca {
-            caches: CaCaches::from_raw(BencodeValue::ByteString(bytes)),
+    pub fn new_byte_string(bytes: ByteString) -> Arc<Ca> {
+        Arc::new(Ca {
+            caches: CaCaches::from_raw(BencodeValue::Bytes(bytes.into())),
             referents: CaReferents::ByteString,
         })
     }
-    pub fn new_list(elements: Vec<ArcIntern<Ca>>) -> ArcIntern<Ca> {
-        ArcIntern::new(Ca {
+    pub fn new_list(elements: Vec<Arc<Ca>>) -> Arc<Ca> {
+        Arc::new(Ca {
             caches: CaCaches::from_raw(BencodeValue::List(
                 elements.iter().map(|ca| ca.caches.id_raw.clone()).collect(),
             )),
             referents: CaReferents::List(elements),
         })
     }
-    pub fn new_map(items: BTreeMap<ArcIntern<Ca>, ArcIntern<Ca>>) -> ArcIntern<Ca> {
-        ArcIntern::new(Ca {
-            caches: CaCaches::from_raw(BencodeValue::Map(
+    pub fn new_map(items: BTreeMap<Arc<Ca>, Arc<Ca>>) -> Arc<Ca> {
+        Arc::new(Ca {
+            caches: CaCaches::from_raw(BencodeValue::Dict(
                 items
                     .iter()
                     .map(|(k, v)| {
                         (
-                            BencodeValue::ByteString(k.id_serialized().data.clone()),
+                            Cow::Owned(k.id_serialized().data.clone()),
                             v.id_raw().clone(),
                         )
                     })
@@ -151,8 +118,32 @@ impl Ca {
     pub fn id_raw(&self) -> &BencodeValue {
         &self.caches.id_raw
     }
-    pub fn id_serialized(&self) -> &ArrayVec<u8, HASH_ID_BENCODE_BYTE_LENGTH> {
+    pub fn id_serialized(&self) -> &SerializedBencode {
         &self.caches.id_serialized
+    }
+}
+
+impl PartialEq for Ca {
+    fn eq(&self, other: &Self) -> bool {
+        self.caches.id_serialized == other.caches.id_serialized
+    }
+}
+impl Eq for Ca {}
+impl PartialOrd for Ca {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.caches
+            .id_serialized
+            .partial_cmp(&other.caches.id_serialized)
+    }
+}
+impl Ord for Ca {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.caches.id_serialized.cmp(&other.caches.id_serialized)
+    }
+}
+impl Hash for Ca {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.caches.id_serialized.hash(state)
     }
 }
 
